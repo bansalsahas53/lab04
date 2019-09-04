d1af7563049be5ee1e5e1f8d959aa578e2e25143
bansalsahas53 <sahas.bansal_cs18@gla.ac.in>
2019-09-04 16:01:43 +0530

    labwork

---
+++ src/lab04/Main.java
@@ -0,0 +1,8 @@
+import lab04.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Person("sahas", 18, 'm');
+        System.out.println(p1);
+    }
+}
---
+++ src/lab04/Person.java
@@ -0,0 +1,49 @@
+package lab04;
+
+public class Person {
+    private String name;
+    private int age;
+    private char gender;
+
+    public Person(String name, int age, char gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder response = new StringBuilder();
+        response.append("hello, my name is :" + getName());
+        response.append(". I am " + getGender());
+        response.append("and i am " + getAge());
+        return response.toString();
+
+    }
+
+}
